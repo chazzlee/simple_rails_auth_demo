8aa68aa5c242e9ca2ced96c6fa1cbab205038ce8
chazzlee <chazz@dev.com>
2022-10-23 15:58:21 -0400

    move current_user to current model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
 
   def login!(user)
-    @current_user = user
+    Current.user = user
     session[:session_token] = user.session_token
   end
 
@@ -16,7 +16,7 @@ class ApplicationController < ActionController::Base
   def current_user
     return nil if session[:session_token].nil?
 
-    @current_user ||= User.find_by(session_token: session[:session_token])
+    Current.user ||= User.find_by(session_token: session[:session_token])
   end
 
   def require_current_user!
